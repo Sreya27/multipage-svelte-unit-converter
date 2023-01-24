c6c850b5cf9e196eaa92f4f6d33a1e7e6b2396a5
Sreya Gadepally <sreyagadepally@gmail.com>
2023-01-24 22:09:21 +0530

    Changed Input Component: added select and option
    Signed-off-by: Sreya Gadepally <sreyagadepally@gmail.com>

--- src/components/Input.svelte
+++ src/components/Input.svelte
@@ -12,45 +12,29 @@
     <h2>{inputHeading}</h2>
     <div class="input-card">
         <div class="input-card-input">
-            <form class="input-form">
-                <input type="number">
-                <div class="dropdown-menu">
-                    <div class="dropdown-header">
-                        <p>{tableItem.units[0]}</p>
-                        <i>&#8964;</i>
-                    </div>
-                    <div class="dropdown-list">
-                        <ul class="input-dropdown">
-                            {#each tableItem.units as unit}
-                                <li id="dropdown-item">{unit}</li>
-                            {/each}
-                        </ul>
-                    </div>
-                </div>
-            </form>
+            <input id="input-value" type="number">
+            <select class="input-dropdown">
+                {#each tableItem.units as unit}
+                    <option value="{unit}">{unit}</option>
+                {/each}
+            </select>
         </div>
         <div class="input-card-output">
-            <form class="output-form">
-                <input type="number">
-                <div class="dropdown-menu">
-                    <div class="dropdown-header">
-                        <p>{tableItem.units[0]}</p>
-                        <i>&#8964;</i>
-                    </div>
-                    <div class="dropdown-list">
-                        <ul class="output-dropdown">
-                            {#each tableItem.units as unit}
-                                <li id="dropdown-item">{unit}</li>
-                            {/each}
-                        </ul>
-                    </div>
-                </div>
-            </form>
+            <input id="output-value" type="number">
+            <select class="output-dropdown">
+                {#each tableItem.units as unit}
+                    <option value="{unit}">{unit}</option>
+                {/each}
+            </select>
         </div>
     </div>
 </div>
 
 <style>
+    .input-card-input, .input-card-output {
+        display: flex;
+        flex-direction: column;
+    }
     .input-card {
         display: flex;
         flex-direction: row;
@@ -70,47 +54,31 @@
         text-align: center;
         margin-top: 0;
     }
-    .input-form, .output-form {
-        border-radius: 15px;
-    }
     input {
         width: 20vw;
         height: 10vw;
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
         border: 0;
+        padding: 0;
     }
     .input-dropdown,.output-dropdown {
+        width: 20vw;
         margin: 0;
         padding: 1%;
         box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
         border: 0;
+        background-color: #eeeeee;
     }
-    .dropdown-header {
-        display: flex;
-        justify-content: space-between;
-        box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-        padding: 1%;
-    }
-    .dropdown-list {
-        display: none;
-    }
-    li {
+    option {
         list-style: none;
     }
-    li:hover {
+    option:hover {
         background-color: white;
     }
     p {
         margin: 1% 0 0 0;
     }
-    .dropdown-menu:hover .dropdown-header {
-        display: none;
-    }
-    .dropdown-menu:hover .dropdown-list {
-        display: block;
-        background-color: #eeeeee;
-    }
-
+    
     @media screen and (min-width: 769px) {
         .input-container {
             min-width: 50%;
@@ -119,3 +87,4 @@
         }
     }
 </style>
+
